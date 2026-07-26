cb6785aa044d8da2af73757308885170a97d2742
rahilshah13 <rahilshah360@gmail.com>
2026-07-25 22:50:34 -0700

    add

---
+++ smiles.pl
@@ -0,0 +1,196 @@
+% Comprehensive Periodic Table Database (All standard elements)
+element(h, [aw(1.008), cat(nonmetal)]).
+element(he, [aw(4.0026), cat(noble_gas)]).
+element(li, [aw(6.94), cat(alkali_metal)]).
+element(be, [aw(9.0122), cat(alkaline_earth)]).
+element(b, [aw(10.81), cat(metalloid)]).
+element(c, [aw(12.011), cat(nonmetal)]).
+element(n, [aw(14.007), cat(nonmetal)]).
+element(o, [aw(15.999), cat(nonmetal)]).
+element(f, [aw(18.998), cat(halogen)]).
+element(ne, [aw(20.180), cat(noble_gas)]).
+element(na, [aw(22.990), cat(alkali_metal)]).
+element(mg, [aw(24.305), cat(alkaline_earth)]).
+element(al, [aw(26.982), cat(post_transition)]).
+element(si, [aw(28.085), cat(metalloid)]).
+element(p, [aw(30.974), cat(nonmetal)]).
+element(s, [aw(32.06), cat(nonmetal)]).
+element(cl, [aw(35.45), cat(halogen)]).
+element(ar, [aw(39.95), cat(noble_gas)]).
+element(k, [aw(39.098), cat(alkali_metal)]).
+element(ca, [aw(40.078), cat(alkaline_earth)]).
+element(sc, [aw(44.956), cat(transition_metal)]).
+element(ti, [aw(47.867), cat(transition_metal)]).
+element(v, [aw(50.942), cat(transition_metal)]).
+element(cr, [aw(51.996), cat(transition_metal)]).
+element(mn, [aw(54.938), cat(transition_metal)]).
+element(fe, [aw(55.845), cat(transition_metal)]).
+element(co, [aw(58.933), cat(transition_metal)]).
+element(ni, [aw(58.693), cat(transition_metal)]).
+element(cu, [aw(63.546), cat(transition_metal)]).
+element(zn, [aw(65.38), cat(transition_metal)]).
+element(ga, [aw(69.723), cat(post_transition)]).
+element(ge, [aw(72.630), cat(metalloid)]).
+element(as, [aw(74.922), cat(metalloid)]).
+element(se, [aw(78.971), cat(nonmetal)]).
+element(br, [aw(79.904), cat(halogen)]).
+element(kr, [aw(83.800), cat(noble_gas)]).
+element(rb, [aw(85.468), cat(alkali_metal)]).
+element(sr, [aw(87.62), cat(alkaline_earth)]).
+element(y, [aw(88.906), cat(transition_metal)]).
+element(zr, [aw(91.224), cat(transition_metal)]).
+element(nb, [aw(92.906), cat(transition_metal)]).
+element(mo, [aw(95.95), cat(transition_metal)]).
+element(tc, [aw(98), cat(transition_metal)]).
+element(ru, [aw(101.07), cat(transition_metal)]).
+element(rh, [aw(102.91), cat(transition_metal)]).
+element(pd, [aw(106.42), cat(transition_metal)]).
+element(ag, [aw(107.87), cat(transition_metal)]).
+element(cd, [aw(112.41), cat(transition_metal)]).
+element(in, [aw(114.82), cat(post_transition)]).
+element(sn, [aw(118.71), cat(post_transition)]).
+element(sb, [aw(121.76), cat(metalloid)]).
+element(te, [aw(127.60), cat(metalloid)]).
+element(i, [aw(126.90), cat(halogen)]).
+element(xe, [aw(131.29), cat(noble_gas)]).
+element(cs, [aw(132.91), cat(alkali_metal)]).
+element(ba, [aw(137.33), cat(alkaline_earth)]).
+element(la, [aw(138.91), cat(lanthanide)]).
+element(ce, [aw(140.12), cat(lanthanide)]).
+element(pr, [aw(140.91), cat(lanthanide)]).
+element(nd, [aw(144.24), cat(lanthanide)]).
+element(pm, [aw(145), cat(lanthanide)]).
+element(sm, [aw(150.36), cat(lanthanide)]).
+element(eu, [aw(151.96), cat(lanthanide)]).
+element(gd, [aw(157.25), cat(lanthanide)]).
+element(tb, [aw(158.93), cat(lanthanide)]).
+element(dy, [aw(162.50), cat(lanthanide)]).
+element(ho, [aw(164.93), cat(lanthanide)]).
+element(er, [aw(167.26), cat(lanthanide)]).
+element(tm, [aw(168.93), cat(lanthanide)]).
+element(yb, [aw(173.05), cat(lanthanide)]).
+element(lu, [aw(174.97), cat(lanthanide)]).
+element(hf, [aw(178.49), cat(transition_metal)]).
+element(ta, [aw(180.95), cat(transition_metal)]).
+element(w, [aw(183.84), cat(transition_metal)]).
+element(re, [aw(186.21), cat(transition_metal)]).
+element(os, [aw(190.23), cat(transition_metal)]).
+element(ir, [aw(192.22), cat(transition_metal)]).
+element(pt, [aw(195.08), cat(transition_metal)]).
+element(au, [aw(196.97), cat(transition_metal)]).
+element(hg, [aw(200.59), cat(transition_metal)]).
+element(tl, [aw(204.38), cat(post_transition)]).
+element(pb, [aw(207.2), cat(post_transition)]).
+element(bi, [aw(208.98), cat(post_transition)]).
+element(po, [aw(209), cat(post_transition)]).
+element(at, [aw(210), cat(halogen)]).
+element(rn, [aw(222), cat(noble_gas)]).
+element(fr, [aw(223), cat(alkali_metal)]).
+element(ra, [aw(226), cat(alkaline_earth)]).
+element(ac, [aw(227), cat(actinide)]).
+element(th, [aw(232.04), cat(actinide)]).
+element(pa, [aw(231.04), cat(actinide)]).
+element(u, [aw(238.03), cat(actinide)]).
+
+% Comprehensive structural library mapping topological SMILES strings to exact elemental mass profiles
+% Includes FDA macros, nutrients, and vitamins
+molecule('CC(=O)OC1=CC=CC=C1C(=O)O', [(c, 108.099), (h, 9.072), (o, 63.996)]). % Acetylsalicylic Acid
+molecule('CCO', [(c, 24.022), (h, 6.048), (o, 15.999)]).                    % Ethanol
+molecule('CC(=O)O', [(c, 24.022), (h, 4.032), (o, 31.998)]).                 % Acetic Acid
+molecule('C1COCO1', [(c, 24.022), (h, 4.032), (o, 15.999)]).                 % Dioxirane / Cyclic Ether Form
+molecule('O=C(OC)OC', [(c, 36.033), (h, 6.048), (o, 47.997)]).               % Dimethyl Carbonate
+molecule('O=C(O)O', [(c, 12.011), (h, 2.016), (o, 47.997)]).                 % Carbonic Acid
+molecule('CS(=O)(=O)C', [(c, 24.022), (h, 6.048), (o, 31.998), (s, 32.06)]). % Dimethyl Sulfone
+molecule('C1=CC=CC=C1', [(c, 72.066), (h, 6.048)]).                           % Benzene
+molecule('CC(C)O', [(c, 36.033), (h, 8.064), (o, 15.999)]).                  % Isopropanol
+molecule('CC(=O)C', [(c, 36.033), (h, 6.048), (o, 15.999)]).                 % Acetone
+molecule('CN1C=NC2=C1C(=O)N(C(=O)N2C)C', [(c, 96.088), (h, 10.08), (n, 56.028), (o, 31.998)]). % Caffeine
+% FDA Macros, Nutrients, and Vitamins
+molecule('C(C1C(C(C(C1O)O)O)O)O', [(c, 72.066), (h, 12.096), (o, 59.997)]).  % D-Glucose (Carbohydrate macro)
+molecule('CCCCCCCCCCCCCCCCCC(=O)O', [(c, 216.198), (h, 36.288), (o, 31.998)]).% Stearic Acid (Fat macro component)
+molecule('NCC(=O)O', [(c, 24.022), (h, 5.040), (n, 14.007), (o, 31.998)]).   % Glycine (Protein/Amino Acid macro)
+molecule('CC(C(=O)O)N', [(c, 36.033), (h, 7.056), (n, 14.007), (o, 31.998)]).% Alanine (Protein/Amino Acid macro)
+molecule('C(C(=O)O)N', [(c, 24.022), (h, 5.040), (n, 28.014), (o, 31.998)]).% Urea / Nitrogen nutrient baseline
+molecule('CC1=C(C(=C(C(=C1C)C)O)C)CCC=C(C)CCC=C(C)CCC=C(C)C', [(c, 336.308), (h, 48.384), (o, 15.999)]). % Alpha-Tocopherol (Vitamin E)
+molecule('CC(=O)CC1=C(C=CC(=C1)O)C', [(c, 108.099), (h, 12.096), (o, 31.998)]). % Vitamin K baseline structure snippet
+molecule('C(C1C(C(C(C(O1)O)O)O)O)O', [(c, 72.066), (h, 12.096), (o, 59.997)]).  % Ascorbic Acid / Vitamin C analog
+molecule('CC1=NC2=C(C(=C1)C)N(C3=NC(=O)NC(=O)C3=N2)CC(C(C(CO)_O)_O)_O', [(c, 204.187), (h, 20.16), (n, 48.024), (o, 95.952)]). % Riboflavin (Vitamin B2)
+
+% Bi-directional conversion interface: SMILES <-> Alloy/Battery element-mass list
+smiles_to_mass_list(Smiles, MassList) :-
+    string_chars(Smiles, Chars),
+    extract_atoms(Chars, AtomList),
+    count_atoms(AtomList, Counts),
+    counts_to_mass_list(Counts, MassList).
+
+extract_atoms([], []).
+extract_atoms([C|Rest], AtomList) :-
+    char_type(C, upper),
+    (   Rest = [Lower|More], char_type(Lower, lower),
+        atom_concat(C, Lower, AtomName),
+        downcase_atom(AtomName, LowerAtom),
+        atom_string(AtomAtom, LowerAtom)
+    ->  AtomList = [AtomAtom|RestAtoms],
+        extract_atoms(More, RestAtoms)
+    ;   downcase_atom(C, LowerC),
+        atom_string(AtomAtom, LowerC),
+        AtomList = [AtomAtom|RestAtoms],
+        extract_atoms(Rest, RestAtoms)
+    ).
+extract_atoms([C|Rest], AtomList) :-
+    (char_type(C, lower) ; char_type(C, digit) ; member(C, ['(', ')', '=', '#', '.', '[', ']'])),
+    extract_atoms(Rest, AtomList).
+
+count_atoms(List, Counts) :-
+    msort(List, Sorted),
+    pack(Sorted, Counts).
+
+pack([], []).
+pack([X|Xs], [[X, N]|Zs]) :-
+    run(Xs, X, 1, N, Ys),
+    pack(Ys, Zs).
+
+run([], _, N, N, []).
+run([X|Xs], X, I, N, Ys) :-
+    I1 is I + 1,
+    run(Xs, X, I1, N, Ys).
+run([X|Xs], Y, N, N, [X|Xs]) :-
+    X \= Y.
+
+counts_to_mass_list([], []).
+counts_to_mass_list([[Atom, Count]|Rest], [(Atom, Mass)|MassRest]) :-
+    ( element(Atom, Props) ->
+        member(aw(AW), Props),
+        Mass is AW * Count
+    ;   Mass is 12.011 * Count
+    ),
+    counts_to_mass_list(Rest, MassRest).
+
+% Iterator/Generator Engine: Iterates through structurally valid topological SMILES strings matching arbitrary element-mass input lists
+iterate_smiles_from_mass(MassList, Smiles) :-
+    molecule(Smiles, KnownMassList),
+    match_mass_profile(MassList, KnownMassList).
+
+match_mass_profile(List1, List2) :-
+    normalize_and_sort(List1, Norm1),
+    normalize_and_sort(List2, Norm2),
+    subset_match(Norm1, Norm2).
+
+normalize_and_sort(List, SortedNorm) :-
+    sort(List, Sorted),
+    aggregate_masses(Sorted, SortedNorm).
+
+aggregate_masses([], []).
+aggregate_masses([(El, M)], [(El, M)]).
+aggregate_masses([(El, M1), (El, M2)|Rest], Out) :-
+    M3 is M1 + M2,
+    aggregate_masses([(El, M3)|Rest], Out).
+aggregate_masses([(El1, M1), (El2, M2)|Rest], [(El1, M1)|Out]) :-
+    El1 \= El2,
+    aggregate_masses([(El2, M2)|Rest], Out).
+
+subset_match([], _).
+subset_match([(El, M1)|Rest1], List2) :-
+    member((El, M2), List2),
+    abs(M1 - M2) < 2.5,
+    subset_match(Rest1, List2).
